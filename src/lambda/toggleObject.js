847a9f07bcfc3d269414e68abe3cefee0cc6361c
user56f857z631fsd2 <omtacjules@gmail.com>
2024-06-14 23:10:01 +0200

    Update toggleObject.js

--- src/lambda/toggleObject.js
+++ src/lambda/toggleObject.js
@@ -1,30 +1,28 @@
-const { createServer } = require('http');
+// src/lambda/toggleObject.js
 const { parse } = require('url');
 const { send } = require('micro');
 
-const PORT = process.env.PORT || 3000;
-
-const server = createServer(async (req, res) => {
-  const { pathname, query } = parse(req.url, true);
+exports.handler = async function(event, context) {
+  const { pathname, query } = parse(event.rawUrl, true);
 
   if (pathname === '/api/toggleObject') {
-    // Extraire la clé API des paramètres de requête
     const apiKey = query.apiKey;
 
-    // Vérifier si la clé API correspond à la variable d'environnement
     if (apiKey && apiKey === process.env.API_KEY) {
-      return send(res, 200, { success: true, message: "Object toggled successfully." });
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, message: "Object toggled successfully." }),
+      };
     } else {
-      return send(res, 401, { success: false, message: "Unauthorized." });
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ success: false, message: "Unauthorized." }),
+      };
     }
   }
 
-  return send(res, 404, { success: false, message: "Not found." });
-});
-
-server.listen(PORT, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`> Ready on http://localhost:${PORT}`);
-});
+  return {
+    statusCode: 404,
+        body: JSON.stringify({ success: false, message: "Not found." }),
+  };
+};
